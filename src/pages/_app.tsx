7d05aa212f12d3c342893d643e9793f7af3dcbfe
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2023-02-14 08:22:43 -0300

    ✨ adding initial files and basic functionality with stripe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
+import Image from 'next/image'
 import type { AppProps } from 'next/app'
 
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
+
+import logoImg from '@/assets/logo.svg'
+
+globalStyles()
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="Ignite Shop Logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
